9f49ae8963dced333f40e1b473109c5e699d32cf
Thierry <thierry.halot@laposte.net>
2018-07-19 23:19:34 +0200

    test de for in avec tableau multidimensionel

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -12,11 +12,18 @@ class TestController extends Controller
      */
     public function index()
     {
+        $personnes = array(
+            1 => array('prenom' => 'Jessy', 'nom' => 'Brown', 'telephone' => array( 'nom'=>'nokia', 'numeros'=>'0658075689')),
+            2 => array('prenom' => 'Sharon', 'nom' => 'Dain', 'telephone' => array( 'nom'=>'samsung', 'numeros'=>'0658075689')),
+            3 => array('prenom' => 'Marta', 'nom' => 'Blanca', 'telephone' => array( 'nom'=>'iphone', 'numeros'=>'0658075689'))
+        );
+
         $test = "helloWorld";
         $tableau = array(
 
             'controller_name' => 'TestController',
             'test'=> $test,
+            'users'=> $personnes
 
     );
         return $this->render('test/index.html.twig', $tableau);
