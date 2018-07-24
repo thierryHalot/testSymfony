28b839a2f4f8ed01b3b9efd08c2b95cf485a2418
Thierry <thierry.halot@laposte.net>
2018-07-24 21:34:14 +0200

    creation des repositorie

---
+++ src/Repository/ApprenantRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Apprenant;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Apprenant|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Apprenant|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Apprenant[]    findAll()
+ * @method Apprenant[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ApprenantRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Apprenant::class);
+    }
+
+//    /**
+//     * @return Apprenant[] Returns an array of Apprenant objects
+//     */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('a.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Apprenant
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+
+}
